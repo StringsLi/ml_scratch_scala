0c4059f9534b6a28980b5ec4592bcd3f2391b1f1
StringsLi <lixin4009@163.com>
2021-01-05 21:36:55 +0800

    正确运行kmeans++

--- src/main/scala/com/strings/model/cluster/KmeansPlus.scala
+++ src/main/scala/com/strings/model/cluster/KmeansPlus.scala
@@ -3,7 +3,6 @@ package com.strings.model.cluster
 import breeze.linalg.{Axis, DenseMatrix, DenseVector, argmax, min, squaredDistance, sum}
 import com.strings.data.Data
 import com.strings.utils.MatrixUtils
-
 import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.util.Random
@@ -21,26 +20,27 @@ class KmeansPlus (val k:Int = 3,
 
   def _init_random_centroids(data : List[DenseVector[Double]]):Unit = {
     val rng  = new Random(seed)
-    val data_copy = data
-    val res1 = rng.shuffle(data_copy).take(1).apply(0)
-    centroids.append(res1)
-    while (centroids.size < k){
-      centroids.append(_choose_next_center(data))
+    for(i <- Range(0,k)){
+      var index = 0
+      if( i == 0) {
+        index = rng.nextInt(data.size)
+      }else{
+        index = _choose_next_center(data)
+      }
+      centroids.append(data(index))
     }
   }
 
-  def _choose_next_center(data : List[DenseVector[Double]]):DenseVector[Double] = {
+  def _choose_next_center(data : List[DenseVector[Double]]):Int = {
     val distance:ArrayBuffer[DenseVector[Double]] = new ArrayBuffer[DenseVector[Double]]()
     for (j <- centroids.indices) {
-      val d = centroids.map(f => MatrixUtils.euclidean_distance(f, centroids(j)))
+      val d = data.map(f => MatrixUtils.euclidean_distance(f, centroids(j)))
       distance.append(DenseVector(d.toArray))
     }
-   val minDistance =  min(DenseMatrix(distance:_*),Axis._0).t
+   val minDistance = min(DenseMatrix(distance:_*),Axis._0).t
    val square_distance = minDistance.map(x => x * x)
    val probs = square_distance.map(x => x / sum(square_distance))
-
-    data.apply(argmax(probs))
-
+    argmax(probs)
   }
 
 
